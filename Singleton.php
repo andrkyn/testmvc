89c4c5ad9b11b0431617cdc15e3e6bb88adf2659
andrkyn <rulex128@rambler.ru>
2018-01-31 13:28:21 +0200

    Singlton example

--- Singleton.php
+++ Singleton.php
@@ -29,10 +29,11 @@ class Singleton {
        через new Singlton клонирование и т.д */
     private function __construct(){}
     private function __clone()    {}
-    private function __wakeup()   {}
+    //private function __wakeup()   {}
 
 }
 Singleton::getInstance();
 Singleton::getInstance();
 Singleton::getInstance();
+
 //  ] END code
