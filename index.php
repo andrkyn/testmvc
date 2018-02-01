1045fe1b3b058582f00b7e245487d1d727581ef2
andrkyn <rulex128@rambler.ru>
2018-02-01 17:52:40 +0200

    Singltone test part2

--- index.php
+++ index.php
@@ -8,30 +8,40 @@
 <body>
 
 <?php
+class db {
 
-$products = array(
-        array(
-            'id'    =>1,
-            'model' =>'LOGITECH KB10', // продукт 1
-            'price' =>'135.67',
-            'type'  =>'keyboard'        // продукт 2
-        ),
-        array(
-            'id'    =>2,
-            'model' =>'A4TECH X56',
-            'price' =>'546.90',
-            'type'  =>'mouse'
-        )
-);
+    public $db;
 
-$productsFactory =new ProductFactory();
-$cart =array();
-foreach ($products as $product) {   // для каждого продукта вызываем метод make
-    $cart[] = $productsFactory->make($product); // передав отдельные массивы в метод make
+    private static $_instance =null;
+    private static function get_instance() {
+        if(self::$_instance instanceof self) {
+            return self::$_instance;
+        }
+        return self::$_instance =new self;
+    }
+    private function __construct() {
+        echo "<h1>Connect to data base</h1>";
+        $this->db =new mysqli('localhost', 'root', 'sql123', 'testDB');
+        if($this->db->connect_error) {
+            throw new DbException("Error connecting: ");
+        }
+
+        $this->db->query("SET NAMES 'UTF8'");
+    }
+
+    private function get_data() {
+        $query = "SELECT * FROM menu";
+        $result = $this->db->query($query);
+        for($i =0; $i<$result->num_rows; $i++) {
+            $row[] =$result->fetch_assoc();
+        }
+        return$row;
+    }
 }
-echo "<pre>";
-var_dump($cart);
-echo "</pre>";
+db::
+//echo "<pre>";
+//var_dump($cart);
+//echo "</pre>";
 
 ?>
 
