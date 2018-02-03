e5a5c41b1be2e4d1f53cde9be6163abf511290bc
andrkyn <rulex128@rambler.ru>
2018-02-03 11:18:45 +0200

    Factory (Ligth example 1)

---
+++ Keyboard.php
@@ -0,0 +1,28 @@
+<?php
+
+class Keyboard {
+    public function __construct()
+    {
+        echo "Это класс - ".__CLASS__;
+    }
+
+    public function init() {
+        $this->i = "True";
+    }
+}
+
+class get {
+    static public function create ($item) {
+        /* тут можно вызывать сколько угодно методов данного объекта
+          для необходимой задачи. И тогда, когда вызываем всеголишь
+          один метод $object =  get::create("parametr") то у нас создается объект return new Keyboard();
+         вызываются требуемые методы и данный объект возвращается */
+        return new Keyboard();
+    }
+}
+
+$object = get::create("parametr");
+
+echo "<pre>";
+var_dump($object);
+echo "</pre>";
--- index.php
+++ index.php
@@ -4,48 +4,14 @@
     <meta charset="UTF-8">
     <title>Document</title>
 </head>
-    <h1>Singlton</h1>
+    <h1>Factory</h1>
 <body>
 
 <?php
-class db {
 
-    public $db;
-
-    private static $_inst =null;
-
-    public static function get_instance() {
-        if(self::$_inst instanceof self) {
-            return self::$_inst;
-        }
-        return self::$_inst =new self;
-    }
-
-    private function __construct() {
-        echo "<h1>Connect to data base</h1>";
-        $this->db =new mysqli('localhost', 'root', 'sql123', 'testDB');
-        if($this->db->connect_error) {
-            throw new DbException("Error connecting: ");
-        }
-
-        $this->db->query("SET NAMES 'UTF8'");
-    }
-
-    private function get_data() {
-        $query = "SELECT * FROM menu";
-        $result = $this->db->query($query);
-        for($i =0; $i<$result->num_rows; $i++) {
-            $row[] =$result->fetch_assoc();
-        }
-        return$row;
-    }
-}
-//db::get_instance();
-//db::get_instance();
-//db::get_instance();
-echo "<pre>";
-var_dump(db::get_instance());
-echo "</pre>";
+//echo "<pre>";
+//var_dump(db::get_instance());
+//echo "</pre>";
 
 ?>
 
